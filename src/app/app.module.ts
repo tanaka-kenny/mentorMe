56b940a33014aef3db00fdd145effa922b4e0687
Perez <tanakakenne2000@gmail.com>
2022-09-25 23:20:34 +0200

    Revert "added firebase setup"
    This reverts commit e653a17107f70ad50f946086f5b126a566d89c98.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { AngularFireModule } from '@angular/fire/compat';
-import { environment } from '../environments/environment'
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule, 
-    IonicModule.forRoot(), 
-    AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig)
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyCrXRyW2d6rfxXDW7GFbwd7-Nvmksf97t4",
-    authDomain: "mentor-me-6ee2e.firebaseapp.com",
-    projectId: "mentor-me-6ee2e",
-    storageBucket: "mentor-me-6ee2e.appspot.com",
-    messagingSenderId: "726802907085",
-    appId: "1:726802907085:web:b079a353e742bbbfd0ce84",
-    measurementId: "G-GD8VJQ0D9N"
-  }
+  production: false
 };
 
 /*
