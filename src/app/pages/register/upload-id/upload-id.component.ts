343bac43bb9aa9f8e7f674b0fb8f87b8cb5a950b
Tanaka <tanakakenne2000@gmail.com>
2023-01-23 13:23:52 +0200

    feature: add upload id screen

--- src/app/pages/register/upload-id/upload-id.component.ts
+++ src/app/pages/register/upload-id/upload-id.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotoService } from 'src/app/services/photo.service';
 
 @Component({
   selector: 'app-upload-id',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadIdComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public photoService: PhotoService) { }
 
   ngOnInit() {}
+
+  selectImage() {
+    this.photoService.selectPhoto();
+  }
 
 }
--- src/app/pages/register/verify/verify.component.ts
+++ src/app/pages/register/verify/verify.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-verify',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerifyComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router) { }
 
   ngOnInit() {}
+
+  nextPage() {
+    this.router.navigate(['register', 'upload','id']);
+  }
 
 }
