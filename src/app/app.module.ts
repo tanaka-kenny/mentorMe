dd0939eea1c5ab3677461e132c9faabdae638a6a
Tanaka <tanakakenne2000@gmail.com>
2023-02-07 20:59:03 +0200

    feature: complete frebase emulators setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,39 @@ import { AppRoutingModule } from './app-routing.module';
 import { USE_EMULATOR as USE_AUTH_EMULATOR } from '@angular/fire/compat/auth';
 import { USE_EMULATOR as USE_FIRESTORE_EMULATOR } from '@angular/fire/compat/firestore';
 import { USE_EMULATOR as USE_FUNCTIONS_EMULATOR } from '@angular/fire/compat/functions';
-import { environment } from 'src/environments/environment';
+import { USE_EMULATOR as USE_STORAGE_EMULATOR } from '@angular/fire/compat/storage';
 
+import { environment } from 'src/environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
+import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
+import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+		provideAuth(() => {
+      connectAuthEmulator(getAuth(), 'http://localhost:9099');
+      return getAuth();
+    }),
+		provideFirestore(() => {
+      connectFirestoreEmulator(getFirestore(), 'localhost', 8080);
+      return getFirestore();
+    }),
+    provideStorage(() => {
+      connectStorageEmulator(getStorage(), 'localhost', 9199);
+      return getStorage();
+    })
+  ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     { provide: USE_AUTH_EMULATOR, useValue: environment.useEmulators ? ['localhost', 9099] : undefined },
     { provide: USE_FIRESTORE_EMULATOR, useValue: environment.useEmulators ? ['localhost', 8080] : undefined },
     { provide: USE_FUNCTIONS_EMULATOR, useValue: environment.useEmulators ? ['localhost', 5001] : undefined },
+    { provide: USE_STORAGE_EMULATOR, useValue: environment.useEmulators ? ['localhost', 9199] : undefined },
   ],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  useEmulators: true
+  useEmulators: true,
+  firebase: {
+    apiKey: 'AIzaSyCrXRyW2d6rfxXDW7GFbwd7-Nvmksf97t4',
+    authDomain: 'mentor-me-6ee2e.firebaseapp.com',
+    projectId: 'mentor-me-6ee2e',
+    storageBucket: 'mentor-me-6ee2e.appspot.com',
+    messagingSenderId: '726802907085',
+    appId: '1:726802907085:web:b079a353e742bbbfd0ce84',
+    measurementId: 'G-GD8VJQ0D9N'
+  }
 };
 
 /*
