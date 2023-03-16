45a6b9c40612ec1a5e69cba4cba4a0328e0ef27a
Tanaka <tanakakenne2000@gmail.com>
2023-03-16 16:19:56 +0200

    add tests for sign-up component

--- src/app/pages/register/sign-up/sign-up.component.spec.ts
+++ src/app/pages/register/sign-up/sign-up.component.spec.ts
@@ -1,18 +1,29 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
+import { By } from '@angular/platform-browser';
+import { AuthorizationService } from 'src/app/services/authorization.service';
 
 import { SignUpComponent } from './sign-up.component';
 
 describe('SignUpComponent', () => {
   let component: SignUpComponent;
   let fixture: ComponentFixture<SignUpComponent>;
+  let authorizationServiceSpy: jasmine.SpyObj<AuthorizationService>;
 
   beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SignUpComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+    const spy = jasmine.createSpyObj('AuthorizationService', [
+      'registerWithEmailAndPassword',
+      'loginWithGoogle',
+      'loginWithFacebook',
+    ]);
 
+    TestBed.configureTestingModule({
+      declarations: [SignUpComponent],
+      imports: [IonicModule.forRoot()],
+      providers: [{ provide: AuthorizationService, useValue: spy }],
+    });
+
+    authorizationServiceSpy = TestBed.inject( AuthorizationService ) as jasmine.SpyObj<AuthorizationService>;
     fixture = TestBed.createComponent(SignUpComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,5 +31,38 @@ describe('SignUpComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#signInWithGoogle', () => {
+    it('should call loginWithGoogle when I click the google icon', () => {
+      const button = fixture.debugElement.query(By.css('.google-icon'));
+
+      button.triggerEventHandler('click');
+
+      expect(authorizationServiceSpy.loginWithGoogle).toHaveBeenCalled();
+    });
+  });
+
+  describe('#signInWithFacebook', () => {
+    it('should call loginWithFacebook when I click the facebook icon', () => {
+      const button = fixture.debugElement.query(By.css('.facebook-icon'));
+
+      button.triggerEventHandler('click');
+
+      expect(authorizationServiceSpy.loginWithFacebook).toHaveBeenCalled();
+    });
+  });
+
+  describe('#createUserWithEmailAndPassword', () => {
+    it('should call login registerWithEmailAndPassword I click the sign-up button', () => {
+      const button = fixture.debugElement.query(By.css('.sign-in-with-password'));
+      const email = component.email;
+      const password = component.password;
+
+      button.triggerEventHandler('click');
+
+      expect(authorizationServiceSpy.registerWithEmailAndPassword)
+        .toHaveBeenCalledWith(email, password);
+    });
   });
 });
--- src/app/services/authorization.service.spec.ts
+++ src/app/services/authorization.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
+import { Auth } from '@angular/fire/auth';
 
 import { AuthorizationService } from './authorization.service';
 
 describe('AuthorizationService', () => {
   let service: AuthorizationService;
+  let ngFireAuthSpy: jasmine.SpyObj<Auth>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const spy = jasmine.createSpyObj('Auth', ['']);
+    TestBed.configureTestingModule({
+      providers: [{ provide: Auth, useValue: spy }],
+    });
+
+    ngFireAuthSpy = TestBed.inject(Auth) as jasmine.SpyObj<Auth>;
     service = TestBed.inject(AuthorizationService);
   });
 
