c2486a68b2c2bf050db85ff61fded3abfe26f600
Tanaka <tanakakenne2000@gmail.com>
2023-05-12 21:03:33 +0200

    move SignUp component to landng module
    this is prevent the sign up page from being protected by the auth guard

--- src/app/pages/landing/landing-routing.module.ts
+++ src/app/pages/landing/landing-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LandingPage } from './landing.page';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LandingPage
+  },
+  {
+    path: 'sign/up',
+    component: SignUpComponent
   }
 ];
 
